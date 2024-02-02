d002fe5496b0acd6379db8bcee2c482330f0cf8c
I-Adarsh-I <adarshsingh9479020444@gmail.com>
2024-02-02 23:55:27 +0530

    Popup bugs fixed

--- frontend-miligram/src/pages/profile/ProfilePage.js
+++ frontend-miligram/src/pages/profile/ProfilePage.js
@@ -1,14 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./profilepage.css";
 import { Button, Modal } from "react-bootstrap";
 
 function ProfilePage() {
-
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
 
   return (
     <div className="container main-profile-page card mt-3">
@@ -66,7 +64,11 @@ function ProfilePage() {
       </div>
       <div className="profile-gallery row g-4">
         <div className="col-md-4 col-sm-12">
-          <div className="card" onClick={handleShow} style={{cursor:'pointer'}}>
+          <div
+            className="card"
+            onClick={handleShow}
+            style={{ cursor: "pointer" }}
+          >
             <img
               className="gallery-pic w-100 rounded"
               alt="Profile pic"
@@ -84,29 +86,63 @@ function ProfilePage() {
           </div>
         </div>
         <div className="col-md-4 col-sm-12">
-            <div className="card">
-          <img
-            className="gallery-pic w-100 rounded"
-            alt="Profile pic"
-            src="https://images.unsplash.com/photo-1494783367193-149034c05e8f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          />
+          <div className="card">
+            <img
+              className="gallery-pic w-100 rounded"
+              alt="Profile pic"
+              src="https://images.unsplash.com/photo-1494783367193-149034c05e8f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
           </div>
         </div>
       </div>
 
-      <Modal show={show} onHide={handleClose}>
-        <div className="container">
-          <div className="row">
-            <div className="col">
-            <img
-            className="gallery-pic w-100 rounded"
-            alt="Profile pic"
-            src="https://images.unsplash.com/photo-1494783367193-149034c05e8f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          />
-          {/* </div> */}
+      <Modal show={show} onHide={handleClose} className="modal" size="lg">
+        <Modal.Header closeButton></Modal.Header>
+        <div className="container row">
+          <div className="col-md-6">
+            <div
+              id="carouselExampleControlsNoTouching"
+              className="carousel slide"
+              data-bs-touch="false"
+            >
+              <div className="carousel-inner mb-3 rounded">
+                <div className="carousel-item active">
+                  <img src="https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                  <img src="https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                  <img src="https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" alt="..." />
+                </div>
+              </div>
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleControlsNoTouching"
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleControlsNoTouching"
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+              </button>
             </div>
-            <div className="col">2</div>
           </div>
+          <div className="col-md-6">Right container</div>
         </div>
       </Modal>
     </div>
